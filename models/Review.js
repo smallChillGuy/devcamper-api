c92c140f1dd2bcf8f8fc872404004699bc4de569
Francisco Jose <lopezozunafranciscojose17@gmail.com>
2022-12-21 18:40:41 +0800

    Aggregate - Calculate Average Rating

--- models/Review.js
+++ models/Review.js
@@ -36,6 +36,40 @@ const ReviewSchema = new mongoose.Schema({
   }
 });
 
+// Static method to get avg rating 
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+  const obj = await this.aggregate([ 
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+	_id: '$bootcamp',
+	averageRating: { $avg: '$rating' }
+      }
+    },
+  ]);
+
+  try {
+   await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: Math.ceil(obj[0].averageRating)
+    })
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Call getAverageRating after save 
+ReviewSchema.post('save',function() {
+   this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageRating before remove 
+ReviewSchema.pre('remove',function() {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
+
 // Prevent user from submitting more than one review for bootcamp 
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
